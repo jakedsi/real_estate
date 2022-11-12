4f0f12ffd041566f180031a05ad657ec29504823
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-12 22:20:19 +0800

    navigation menus

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='h-screen bg-[#2C2D2A] text-white'>
+        <div className='w-full flex flex-col items-center'>
+            <h1 className='w-[50%] text-center mt-[20vh] text-[50px] font-semibold'>Discover A Place You'll Love To Live</h1>
+        </div>
+    </div>
+  )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,16 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        <div className='flex justify-between px-5'>
-            <div>AnteLangga</div>
-            <ul className='flex'>
-                <li>Home</li>
-                <li>Buy</li>
+        <div className='flex justify-between px-5 absolute w-full py-5 text-white items-center'>
+            <div className='text-[20px] font-medium'>JosephineParilla</div>
+            <ul className='flex text-[15px] gap-10 font-light'>
+                <li className='font-normal'>Home</li>
                 <li>About</li>
                 <li>Pricing</li>
             </ul>
-            <div className='flex'>
-                <button>Login</button>
-                <button>Signup</button>
+            <div className='flex font-light text-[14px] gap-3'>
+                <button className='px-5 py-3'>Login</button>
+                <button className='px-5 py-3 bg-blue-500 rounded-lg'>Signup</button>
             </div>
         </div>
     </div>
