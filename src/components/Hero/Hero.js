a282110f300de3640dab109b7f5cfdad073acd07
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-13 14:58:34 +0800

    hero section responsive

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import HeroImg from '../../assets/img/hero.png'
 export default function Hero() {
   return (
     <div className='h-screen bg-[#2C2D2A] text-white'>
         <div className='w-full flex flex-col items-center'>
-            <h1 className='w-[50%] text-center mt-[20vh] text-[50px] font-semibold'>Discover A Place You'll Love To Live</h1>
+            <h1 className='w-[600px] text-center mt-[20vh] text-[50px] font-semibold sm:w-[90%] sm:text-[40px]'>Discover A Place You'll Love To Live</h1>
+            <h1 className='w-[600px] text-center mt-[5vh] text-[15px] sm:w-[90%] sm:text-[12px]'>Find comfort in the house with us, want to find a home? we are ready to help you wholeheartedly based on what you need</h1>
+            <button className='px-5 py-2 mt-[5vh] bg-blue-500 rounded-lg sm:py-[3px] sm:text-[12px]'>Get Started</button>
+            <img src={HeroImg} alt="hero" className='w-[900px] h-[400px] mt-[10vh] object-cover rounded-[54px] sm:w-[95%]' />
         </div>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav>
         <div className='flex justify-between px-5 absolute w-full py-5 text-white items-center'>
             <div className='text-[20px] font-medium'>JosephineParilla</div>
-            <ul className={nav ? 'flex text-[15px] gap-10 font-light md:absolute md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-0 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-full md:transition md:duration-500' : 'flex text-[15px] gap-10 font-light md:absolute md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-0 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-0 md:transition md:duration-500'}>
+            <ul className={nav ? 'flex text-[15px] gap-10 font-light md:fixed md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-0 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-full md:transition md:duration-500' : 'flex text-[15px] gap-10 font-light md:fixed md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-0 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-0 md:transition md:duration-500'}>
                 
                 <li className={nav ? 'md:opacity-0 md:transition-opacity' : 'md:opacity-100 md:transition-opacity md:duration-[1000ms]'}>Home</li>
                 <li className={nav ? 'md:opacity-0 md:transition-opacity' : 'md:opacity-100 md:transition-opacity md:duration-[1500ms]'}>About</li>
@@ -17,10 +17,10 @@ export default function Navbar() {
                 </div>
             </ul>
             <div className='flex font-light text-[14px] gap-3 md:hidden'>
-                <button className='px-5 py-3'>Login</button>
-                <button className='px-5 py-3 bg-blue-500 rounded-lg'>Signup</button>
+                <button className='px-5 py-2'>Login</button>
+                <button className='px-5 py-2 bg-blue-500 rounded-lg'>Signup</button>
             </div>
-            <div className='hidden md:flex flex-col gap-1 z-10 cursor-pointer' onClick={()=>navIsActive(prev => !prev)}>
+            <div className={nav ? 'hidden md:flex flex-col gap-1 z-10 cursor-pointer absolute right-[15px]' : 'hidden md:flex flex-col gap-1 z-10 cursor-pointer fixed right-[15px]'} onClick={()=>navIsActive(prev => !prev)}>
                 <div className={nav ? 'bg-white h-[2px] w-5 transition-all' : 'bg-white h-[2px] w-5 rotate-[-45deg] translate-x-[-5px] translate-y-[6px] transition-all'}></div>
                 <div className={nav ? 'bg-white h-[2px] w-5 transition-all' : 'bg-white h-[2px] w-5 opacity-0'}></div>
                 <div className={nav ? 'bg-white h-[2px] w-5 transition-all' : 'bg-white h-[2px] w-5 rotate-[45deg] translate-x-[-5px] translate-y-[-6px] transition-all'}></div>
