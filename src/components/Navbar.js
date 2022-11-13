7bfca9a14b1aa43167f8fbb4ed773659d7fabd4b
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-13 22:31:39 +0800

    quick fixed navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,12 @@ export default function Navbar() {
     <nav>
         <div className='flex justify-between px-5 absolute w-full py-5 text-white items-center'>
             <div className='text-[20px] font-medium'>JosephineParilla</div>
-            <ul className={nav ? 'flex text-[15px] gap-10 font-light md:fixed md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-0 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-full md:transition md:duration-500' : 'flex text-[15px] gap-10 font-light md:fixed md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-0 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-0 md:transition md:duration-500'}>
+            <ul className={nav ? 'flex text-[15px] gap-10 font-light md:fixed md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-10 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-full md:transition md:duration-500' : 'flex text-[15px] gap-10 font-light md:fixed md:right-0 md:top-0 md:h-screen md:bg-[#393a37] md:w-[250px] md:z-10 md:pt-[10vh] md:flex-col md:items-center md:justify-start md:translate-x-0 md:transition md:duration-500'}>
                 
                 <li className={nav ? 'md:opacity-0 md:transition-opacity' : 'md:opacity-100 md:transition-opacity md:duration-[1000ms]'}>Home</li>
                 <li className={nav ? 'md:opacity-0 md:transition-opacity' : 'md:opacity-100 md:transition-opacity md:duration-[1500ms]'}>About</li>
                 <li className={nav ? 'md:opacity-0 md:transition-opacity' : 'md:opacity-100 md:transition-opacity md:duration-[2000ms]'}>Pricing</li>
-                <div className='md:flex font-light text-[12px] gap-3 hidden my-5'>
+                <div className='md:flex flex-col-reverse font-light text-[12px] gap-3 hidden my-5'>
                     <button className='px-4 py-2'>Login</button>
                     <button className='px-4 py-2 bg-blue-500 rounded-lg'>Signup</button>
                 </div>
@@ -20,7 +20,7 @@ export default function Navbar() {
                 <button className='px-5 py-2'>Login</button>
                 <button className='px-5 py-2 bg-blue-500 rounded-lg'>Signup</button>
             </div>
-            <div className={nav ? 'hidden md:flex flex-col gap-1 z-10 cursor-pointer absolute right-[15px]' : 'hidden md:flex flex-col gap-1 z-10 cursor-pointer fixed right-[15px]'} onClick={()=>navIsActive(prev => !prev)}>
+            <div className={nav ? 'hidden md:flex flex-col gap-1 z-20 cursor-pointer absolute right-[15px]' : 'hidden md:flex flex-col gap-1 z-20 cursor-pointer fixed right-[15px]'} onClick={()=>navIsActive(prev => !prev)}>
                 <div className={nav ? 'bg-white h-[2px] w-5 transition-all' : 'bg-white h-[2px] w-5 rotate-[-45deg] translate-x-[-5px] translate-y-[6px] transition-all'}></div>
                 <div className={nav ? 'bg-white h-[2px] w-5 transition-all' : 'bg-white h-[2px] w-5 opacity-0'}></div>
                 <div className={nav ? 'bg-white h-[2px] w-5 transition-all' : 'bg-white h-[2px] w-5 rotate-[45deg] translate-x-[-5px] translate-y-[-6px] transition-all'}></div>
