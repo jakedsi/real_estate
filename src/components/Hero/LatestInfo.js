0a10ba7f4c4f9d3cc6c9c5b9361a2f352d83f1ed
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-15 12:40:38 +0800

    responsive latest-info

--- src/components/Hero/LatestInfo.js
+++ src/components/Hero/LatestInfo.js
@@ -7,8 +7,8 @@ export default function LatestInfo() {
   return (
     <div className='py-[10vh] bg-[#F2F2F2]'>
         <div className='flex justify-center'>
-            <div className='w-[900px] flex justify-between gap-[5rem] items-center md:flex-col-reverse md:gap-2'>
-                <h1 className='text-[30px] text-[#030E47] font-semibold'>Latest Housing Information</h1>
+            <div className='w-[900px] flex justify-between gap-[5rem] items-center md:flex-col md:gap-2'>
+                <h1 className='text-[30px] text-[#030E47] font-semibold text-center'>Latest Housing Information</h1>
                 <h1 className='text-[15px] text-gray-500 flex justify-center gap-5'>Latest Real Estate Blogs<br/>
                 <button className='px-5 py-1 bg-blue-500 rounded-lg text-[11px] text-white text-center mx-auto'>See All Articles</button>
                 </h1>
@@ -16,7 +16,7 @@ export default function LatestInfo() {
         </div>
         <div className='flex justify-center mt-[5vh]'>
             <div className='w-[900px] flex flex-wrap gap-5 justify-around'>
-                <div className='w-[30%] h-[320px] flex flex-col p-2 justify-start items-start'>
+                <div className='w-[30%] h-[320px] flex flex-col p-2 justify-start items-start sm:w-[300px] 1xs:w-full 1xs:px-3'>
                     <img src={Latest1} alt="" className='rounded-[20px] w-full h-[150px] object-cover' />
                     <div className='pt-2 py-1 flex items-center border-b border-[#796e52]'>
                         <BsDot  fontSize="15px" className='text-[#866e2b]'/>
@@ -25,7 +25,7 @@ export default function LatestInfo() {
                     <h1 className='pr-3 mt-3'>Home Sales About To Surge? We Many See a Summer Like Never Before.</h1>
                     <button className='px-4 py-1 bg-blue-500 rounded-lg text-[11px] text-white text-center mt-auto'>Read More</button>
                 </div>
-                <div className='w-[30%] h-[320px] flex flex-col p-2 justify-start items-start'>
+                <div className='w-[30%] h-[320px] flex flex-col p-2 justify-start items-start sm:w-[300px] 1xs:w-full 1xs:px-3'>
                     <img src={Latest2} alt="" className='rounded-[20px] w-full h-[150px] object-cover' />
                     <div className='pt-2 py-1 flex items-center border-b border-[#796e52]'>
                         <BsDot  fontSize="15px" className='text-[#866e2b]'/>
@@ -34,7 +34,7 @@ export default function LatestInfo() {
                     <h1 className='pr-3 mt-3'>Retiretment May Be Changing What You Need In a Home</h1>
                     <button className='px-4 py-1 bg-blue-500 rounded-lg text-[11px] text-white text-center mt-auto'>Read More</button>
                 </div>
-                <div className='w-[30%] h-[320px] flex flex-col p-2 justify-start items-start'>
+                <div className='w-[30%] h-[320px] flex flex-col p-2 justify-start items-start sm:w-[300px] 1xs:w-full 1xs:px-3'>
                     <img src={Latest3} alt="" className='rounded-[20px] w-full h-[150px] object-cover' />
                     <div className='pt-2 py-1 flex items-center border-b border-[#796e52]'>
                         <BsDot  fontSize="15px" className='text-[#866e2b]'/>
