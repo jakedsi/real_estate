efba78d865f07b73c1d361aa7f12e3dc976b02e1
jakeaboyot <jakeaboyot0@gmail.com>
2022-11-15 10:55:45 +0800

    new hero bg-color

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroImg from '../../assets/img/hero.jpg'
 export default function Hero() {
   return (
-    <div className='h-screen bg-[#2C2D2A] text-white'>
+    <div className='h-screen bg-[#25492e] text-white'>
         <div className='w-full flex flex-col items-center'>
             <h1 className='w-[600px] text-center mt-[20vh] text-[50px] font-semibold sm:w-[90%] sm:text-[40px]'>Discover A Place You'll Love To Live</h1>
             <h1 className='w-[600px] text-center mt-[5vh] text-[15px] sm:w-[90%] sm:text-[12px]'>Find comfort in the house with us, want to find a home? we are ready to help you wholeheartedly based on what you need</h1>
